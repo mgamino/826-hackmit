c7e4f113018cb7681ca304986b784be454b2f986
Megan Gamino <megangamino@Megans-MacBook-Pro-5.local>
2017-09-16 15:44:38 -0400

    todos

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from google.appengine.api import users
 from google.appengine.ext import ndb
 import logging
 
+#TODO: finish figuring out the profile bullshit lmao
+
+
 class Profile (ndb.Model):
     name = ndb.StringProperty()
     bio = ndb.TextProperty()
@@ -40,6 +43,8 @@ class MainHandler(webapp2.RequestHandler):
             email = user.email().lower()
             logout_url=users.CreateLogoutURL('/')
 
+            #TODO: if profile w/ matching email does not exist, create profile object with name, bio, and others set as some boring defaults (think uber driver profiles)
+
             template_vals = {'email':email, 'logout_url':logout_url}
             template = jinja_environment.get_template("main.html")
             self.response.write(template.render(template_vals))
@@ -50,16 +55,30 @@ class MainHandler(webapp2.RequestHandler):
 
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+        user_email = user.email()
+        #TODO: query for profile key with this email
+        logout_url=users.CreateLogoutURL('/')
+
+        #TODO: query stories for user key (email?) is this one!
+        #TODO: actually do that twice, once for published and one not published
+        #TODO: jk make it 3, one for published, one for finished not published (awaiting approval), and one for in progress (so u can get the story key from URL)
+
+
         template = jinja_environment.get_template("profile.html")
-        self.response.write(template.render())
+        template_vals = {'logout_url':logout_url}
+        self.response.write(template.render(template_vals))
 
 class SetProfileHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template("setprofile.html")
         self.response.write(template.render())
 
+    #TODO: def post(self) with fields
+
 class ReadHandler(webapp2.RequestHandler):
     def get(self):
+        #TODO: query stories where published == true
         template = jinja_environment.get_template("read.html")
         self.response.write(template.render())
 
@@ -80,13 +99,34 @@ class WriteHandler(webapp2.RequestHandler):
 
 class FreewriteHandler(webapp2.RequestHandler):
     def get(self):
+        #TODO: something about loading the story key?? from url maybe??
         template = jinja_environment.get_template("freewrite.html")
         self.response.write(template.render())
+    #TODO: def post(self): save the card & story
 
 class CyoaHandler(webapp2.RequestHandler):
     def get(self):
+        #TODO: template_vals for user + story_key
         template = jinja_environment.get_template("cyoa.html")
         self.response.write(template.render())
+    def post(self):
+        one = self.request.get("one")
+        one_one = self.request.get("one_one")
+        one_one_one = self.request.get("one_one_one")
+
+        one_one_two = self.request.get("one_one_two")
+
+        one_two = self.request.get("one_two")
+        one_two_one = self.request.get("one_two_one")
+
+        one_two_two = self.request.get("one_two_two")
+
+        two = self.request.get("two")
+        two_one = self.request.get("two_one")
+        two_two = self.request.get("two_two")
+
+        #TODO(megan): card.put!! for all of these also get story keY>??
+
 
 class SubmitHandler(webapp2.RequestHandler):
     def get(self):
